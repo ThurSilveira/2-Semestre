ea21e6426bcffcb84b95b3c6a9f6bd469403d1a0
ThurSilveira <arthursilveira13000@gmail.com>
2025-08-09 11:05:12 -0300

    Adicionando exercicio de revisão e alterando nome do arquivo

--- Daniel/2° Semestre/Exercicio Revisão Vetores/js/objeto.js
+++ Daniel/2° Semestre/Exercicio Revisão Vetores/js/objeto.js
@@ -1,57 +1,25 @@
-
-// Exemplo de objeto em JS
+// Revisão
 
 function exe(){
-    let jogos = [{
-        titulo: "God of war Ragnarok",
-        editora: "Santa Monica Studio",
-        lancamento: 2022
-        },
-        {
-        titulo: "God of war",
-        editora: "Santa Monica Studio",
-        ano: 2018
-        },
-        {
-        titulo: "God of war III",
-        editora: "Santa Monica Studio",
-        ano: 2015
-        },
-        {
-        titulo: "God of war II",
-        editora: "Santa Monica Studio",
-        ano: 2007
-        },
-        {
-        titulo: "God of war I",
-        editora: "Santa Monica Studio",
-        ano: 2005
+    let vet = []
+    let pares = 0, impares = 0, cont = 0
+    let med, num
+    for(let i=0;i<6;i++){
+        num = Number(prompt("Digite um número:"))
+        vet.push(num)
+        if(vet[i] % 2 === 0){
+            pares++
+            cont += vet[i]
         }
-    ];
-
-console.log(jogos[4].ano); // para exibir  determinado objeto
-
-for(let i=0; i<jogos.length;i++){
-    console.log(jogos[i].ano) // para exibir todos anos
-};
-
-// Exemplo de criar objeto para vetores
-
-let gamepass = [];
-
-for (let i = 0; i < 3; i++) {
-    let titulo = prompt(`Informe o título do jogo ${i + 1}:`);
-    let autor = prompt(`Informe a editora do jogo ${i + 1}:`);
-    let ano = Number(prompt(`Informe o ano de lançamento do jogo ${i + 1}:`));
-
-    let games = {
-        titulo: titulo,
-        autor: autor,
-        ano: ano
-    };
-    gamepass.push(games);
+        else{
+            impares++
+        }
+    }
+    if(pares > 0){
+        med = cont / pares
+    }
+    console.log(impares)
+    console.log(pares)
+    console.log(med)
+    console.log(vet)
 }
-
-console.log(gamepass)
-
-};
