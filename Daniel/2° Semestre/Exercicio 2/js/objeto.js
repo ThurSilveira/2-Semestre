f2d734097e67c59d818609cff5b1ff6cc59bf09f
ThurSilveira <arthursilveira13000@gmail.com>
2025-08-16 13:48:23 -0300

    Exercicio 2 Completo

--- Daniel/2° Semestre/Exercicio 2/js/objeto.js
+++ Daniel/2° Semestre/Exercicio 2/js/objeto.js
@@ -1,18 +1,15 @@
 // Atividade 2
-
-function exe(){
+function exe() {
     let empresa = []
 
-    // Lendo nomes de funcionarios
+    // Lendo nomes de funcionários
+    for (let i=0; i<3; i++) {
+        let nome = prompt(`Informe o nome do ${i+1}º funcionário:`)
+        let cargo = prompt(`Informe o cargo de ${nome}:`).toUpperCase()
+        let salario = Number(prompt(`Informe o salário de ${nome}:`))
+        let tempoServico = Number(prompt(`Informe o tempo de serviço (anos) de ${nome}:`))
 
-    for(let i = 0; i<3; i++){
-        let nome = prompt(`Informe o nome do ${i+1} funcionario:`)
-        let cargo = prompt(`Informe o cargo de ${nome}:`).toLocaleLowerCase()
-        let salario = Number(prompt(`Informe o salario de ${nome}:`))
-        let tempoServico = Number(prompt(`Informe o tempo de servico em anos de ${nome}:`))
-
-    // Adicionando objeto ao vetor
-
+        // Adicionando objeto ao vetor
         let funcionario = {
             nome: nome,
             cargo: cargo,
@@ -22,35 +19,35 @@ function exe(){
 
         empresa.push(funcionario)
     }
-    console.log(empresa)
-    // Resultados do exercicio
 
-    let somaSalarios = 0, med
+    console.log("Lista de funcionários:", empresa)
+
+    // Resultados do exercício
+    let somaSalarios = 0
     let maiorSalario = empresa[0]
     let contGerentes = 0
 
-
-    for (let i = 0; i<empresa.length; i++){
-        somaSalarios += empresa.salario[i]
-
-        if (empresa.salario[i] > 5000 && empresa.tempoServico[i] > 5){
-            console.log(`Funcionarios com salario superior a 5 mil e com mais de 5 anos de empresa: ${empresa.nome}`)
+    for (let i =0; i<empresa.length; i++) {
+        let func = empresa[i]
+        if (func.salario > 5000 && func.tempoServico > 5) {
+            console.log(`Funcionário com salário > 5 mil e mais de 5 anos: ${func.nome}`)
         }
 
-        if (empresa.salario[i] > maiorSalario){
-            maiorSalario = empresa[i]
+        if (func.salario > maiorSalario.salario) {
+            maiorSalario = func
         }
 
-        if (empresa.cargo === "gerente" .toLocaleLowerCase()){
+        if (func.cargo === "GERENTE") {
             contGerentes++
         }
 
+        somaSalarios += func.salario
     }
-    med = somaSalarios / empresa.length
+
+    let media = somaSalarios / empresa.length
 
     // Exibindo Resultados
-    console.log(maiorSalario)
-    console.log(med)
-    console.log(contGerentes)
-
+    console.log("Funcionário com maior salário:", maiorSalario.nome, "-", maiorSalario.salario)
+    console.log("Média dos salários:", media)
+    console.log("Quantidade de gerentes:", contGerentes)
 }
